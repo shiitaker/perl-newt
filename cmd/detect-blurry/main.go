8b360fcbffe22df07efae575d6b284c298ad9e77
shiitaker <105961065+shiitaker@users.noreply.github.com>
2021-05-18 11:41:29 +0000

    v3.3.0

--- cmd/detect-blurry/main.go
+++ cmd/detect-blurry/main.go
@@ -27,3 +27,10 @@ func main() {
 	result := gocv.NewMat()
 	defer result.Close()
 	mean := gocv.NewMat()
+	defer mean.Close()
+
+	gocv.Laplacian(img, &laplacian, img.Type(), 5, 1, 0, gocv.BorderDefault)
+	gocv.MeanStdDev(laplacian, &mean, &result)
+	deviation := result.Mean().Val1 * result.Mean().Val1
+	if deviation < thresold {
+		fmt.Println("Blurry", deviation, *source)
