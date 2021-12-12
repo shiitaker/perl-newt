701703bbb93060bd27fd1f09a98c0248cdb0f6b6
shiitaker <105961065+shiitaker@users.noreply.github.com>
2021-12-12 18:46:57 +0000

    Working Version

--- cmd/ml-image-tile/tools.go
+++ cmd/ml-image-tile/tools.go
@@ -7,4 +7,4 @@ import (
 	"github.com/go-kit/kit/log/level"
 )
 
-// NewLevelFilterFromString filter the log level using the string "DEBUG|INFO|WARN|ER
+// NewLevelFilterFromString filter the log level using the string "DEBUG|INFO|WARN|ERROR
