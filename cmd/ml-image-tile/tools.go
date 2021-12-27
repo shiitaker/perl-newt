dbab8028c96a528eff899f690446cb340c2f29cd
shiitaker <105961065+shiitaker@users.noreply.github.com>
2021-12-27 06:16:00 +0000

    fix vari

--- cmd/ml-image-tile/tools.go
+++ cmd/ml-image-tile/tools.go
@@ -8,4 +8,4 @@ import (
 )
 
 // NewLevelFilterFromString filter the log level using the string "DEBUG|INFO|WARN|ERROR"
-func NewLevelFilterFromString(next log.Logger, ls string) log.
+func NewLevelFilterFromString(next log.Logger, ls string) log.L
