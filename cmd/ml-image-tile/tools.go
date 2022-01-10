8d12882fbe6f5a9f830565f14b6c5ed0c529ae2b
shiitaker <105961065+shiitaker@users.noreply.github.com>
2022-01-10 10:03:34 +0000

    Add updated cecil submodule

--- cmd/ml-image-tile/tools.go
+++ cmd/ml-image-tile/tools.go
@@ -9,4 +9,4 @@ import (
 
 // NewLevelFilterFromString filter the log level using the string "DEBUG|INFO|WARN|ERROR"
 func NewLevelFilterFromString(next log.Logger, ls string) log.Logger {
-	switch str
+	switch strings.T
