56e3cea9a69fce2633f753b17f3350bf49c10fee
shiitaker <105961065+shiitaker@users.noreply.github.com>
2022-01-17 12:02:02 +0000

    New translations messages.json (Portuguese, Brazilian)

--- cmd/ml-image-tile/tools.go
+++ cmd/ml-image-tile/tools.go
@@ -9,4 +9,4 @@ import (
 
 // NewLevelFilterFromString filter the log level using the string "DEBUG|INFO|WARN|ERROR"
 func NewLevelFilterFromString(next log.Logger, ls string) log.Logger {
-	switch strings.ToLower(ls
+	switch strings.ToLower(ls) {
